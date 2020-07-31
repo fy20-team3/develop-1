73f08a1d8255fdebcaf4ef372046c8f2b8238e82
taiseikato <68930969+taiseikato@users.noreply.github.com>
2020-07-31 15:48:05 +0900

    Update settings.py

--- develop/settings.py
+++ develop/settings.py
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#ログイン後に飛ぶページの設定URL
+LOGIN_URL = 'mymap:login'
+LOGIN_REDIRECT_URL = 'mymap:index'
