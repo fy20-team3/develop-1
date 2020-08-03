401d6d9333f772253024c22b81b8c9169aefd84b
MahoHoshino <68933298+MahoHoshino@users.noreply.github.com>
2020-08-03 15:34:37 +0900

    Update urls.py

--- localmap/urls.py
+++ localmap/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.viewspot, name='viewspot'),
-    path('hotelspot', views.hotelspot,name='hotelspot')
+    path('', views.entrance, name='entrance'),
+    path('viewspot', views.viewspot, name='viewspot'),
+    path('hotelspot', views.hotelspot,name='hotelspot'),
+    path('tripplan', views.tripplan,name='tripplan')
 ]
