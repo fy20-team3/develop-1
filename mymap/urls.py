50dd904c2a3d4d16ad700024615cda00d6aae537
taiseikato <68930969+taiseikato@users.noreply.github.com>
2020-08-03 15:34:57 +0900

    Update urls.py

--- mymap/urls.py
+++ mymap/urls.py
@@ -5,8 +5,10 @@ from . import views
 app_name = 'mymap'
 
 urlpatterns = [
- path('', views.index, name='index'),
- path('signup/', views.SignUp.as_view(), name='signup'), 
- path('login/', views.Login.as_view(), name='login')
-
+path('', views.index, name='index'),
+path('signup/', views.SignUp.as_view(), name='signup'), 
+path('login/', views.Login.as_view(), name='login'),
+path('overview/', views.Overview, name='overview'),
+path('logout/', views.Logout.as_view(), name='logout'),
+path('mypage/', views.Mypage, name='mypage'),
 ]
