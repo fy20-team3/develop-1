b92b4925139cc9152f5196d6b79dc2774ee61d8b
Shintaro0122 <68933883+Shintaro0122@users.noreply.github.com>
2020-08-03 22:49:26 +0900

    Update settings.py

--- develop/settings.py
+++ develop/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = (
 # [
@@ -142,3 +142,4 @@ STATIC_URL = '/static/'
 LOGIN_URL = 'mymap:login'
 LOGIN_REDIRECT_URL = 'mymap:index'
 LOGOUT_REDIRECT_URL = 'mymap:index'
+
